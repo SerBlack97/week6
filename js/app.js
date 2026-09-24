86b0af9cbc234a8a16ac6de8dfa5e813cc68c0f7
SerBlack97 <serblack@iu.edu>
2026-09-24 10:34:53 -0400

    push

--- js/app.js
+++ js/app.js
@@ -1,35 +1,43 @@
-function mathSum (){
-    console.log(num)
-    const sum = Number(document.getElementById("number1").value) + Number(document.getElementById("number2").value);
-    return sum;
+const calculate = document.getElementById("calculate");
+const results = document.getElementById("output");
+
+function handleSubmit(event){
+    event.preventDefault();
+    console.log(document.getElementById("operation").value);
+    document.querySelector(".box").innerHTML=mathSum();
 }
+// function mathSum (){
+//     console.log(num)
+//     const sum = Number(document.getElementById("number1").value) + Number(document.getElementById("number2").value);
+//     return sum;
+// }
 
-console.log(mathSum());
+// console.log(mathSum());
 
-function mathSub (){
-    console.log(num)
-    const sum = Number(document.getElementById("number1").value) - Number(document.getElementById("number2").value);
-    return sum;
-}
+// function mathSub (){
+//     console.log(num)
+//     const sum = Number(document.getElementById("number1").value) - Number(document.getElementById("number2").value);
+//     return sum;
+// }
 
-console.log(mathSub());
+// console.log(mathSub());
 
-function mathMul (){
-    console.log(num)
-    const sum = Number(document.getElementById("number1").value) - Number(document.getElementById("number2").value);
-    return sum;
-}
+// function mathMul (){
+//     console.log(num)
+//     const sum = Number(document.getElementById("number1").value) - Number(document.getElementById("number2").value);
+//     return sum;
+// }
 
-console.log(mathMul());
+// console.log(mathMul());
 
-function mathDivi (){
-    console.log(num)
-    const sum = Number(document.getElementById("number1").value) - Number(document.getElementById("number2").value);
-    return sum;
-}
+// function mathDivi (){
+//     console.log(num)
+//     const sum = Number(document.getElementById("number1").value) - Number(document.getElementById("number2").value);
+//     return sum;
+// }
 
-console.log(mathDivi());
+// console.log(mathDivi());
 
 
-document.querySelector("button").addEventListener("click", function() {document.querySelector("#special").innerHTML=mathSum();
-})
+// document.querySelector("output").addEventListener("click", function() {document.querySelector("#special").innerHTML=mathSum();
+// })
